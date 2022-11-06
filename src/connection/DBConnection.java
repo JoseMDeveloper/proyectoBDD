c4418e605db191465d133e06bcb78bb42cd5731d
Santicm23 <santicastrom@outlook.com>
2022-11-06 15:59:20 -0500

    guardando DDL

--- src/connection/DBConnection.java
+++ src/connection/DBConnection.java
@@ -3,16 +3,27 @@ package connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBConnection {
-	private static Connection connection = null;
+	private static Connection connection;
+	private static Statement statement;
     private static String login = "is1270204";
     private static String password  ="qOqI64e0LT";
     private static String url = "jdbc:oracle:thin:@orion.javeriana.edu.co:1521/LAB";
+    
+    public static Connection getConnection() {
+		return connection;
+    }
+    
+    public static Statement getStatement() {
+		return statement;
+    }
 
-    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+    public static void connect() throws ClassNotFoundException, SQLException {
         try {
             connection = DriverManager.getConnection(url, login, password);
+            statement = connection.createStatement();
             //conn.setAutoCommit(false);
             if(connection != null) {
                 System.out.println("Conexion exitosa");
@@ -26,10 +37,9 @@ public class DBConnection {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        return connection;
     }
 
-    public static void desconectar() {
+    public static void closeConnection() {
         try {
             connection.close();
         }
--- src/connection/Queries.java
+++ src/connection/Queries.java
@@ -1,15 +1,54 @@
 package connection;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Queries {
-	public static void TotPropiedadesClientePeriodos() {
-		
+	public static ResultSet totPropiedadesPorClientePorPeriodo() throws SQLException {
+		//Cantidad Total de propiedades rentadas por cliente por períodos (mes, año)
+		String query = "";
+		return DBConnection.getStatement().executeQuery(query);
 	}
 	
-	public static void TotPapadoDueñosPeriodos() {
-		
+	public static ResultSet totPapadoDuenoPorPeriodo() throws SQLException {
+		//Suma total pagada a los dueños por las rentas de sus propiedades (suma del valor de renta estipulado) por períodos
+		String query = "";
+		return DBConnection.getStatement().executeQuery(query);
 	}
 
-	public static void TotRentasClientePeriodos() {
-		
+	public static ResultSet totRentasPorClientePorPeriodo() throws SQLException {
+		//Suma y cantidad de rentas por cliente, por período
+		String query = "";
+		return DBConnection.getStatement().executeQuery(query);
+	}
+	
+	public static ResultSet totRentasPorUbicacion() throws SQLException {
+		//Número total de rentas por país, departamento, municipio y ubicación (location)
+		String query = "";
+		return DBConnection.getStatement().executeQuery(query);
+	}
+	
+	public static ResultSet impuestosAPagarPorRentaPorPeriodo() throws SQLException {
+		//Reporte por periodos (año, mes) de Impuestos que se deben pagar por cada renta
+		String query = "";
+		return DBConnection.getStatement().executeQuery(query);
+	}
+	
+	public static ResultSet topPropiedadesRentadas() throws SQLException {
+		//¿Cuáles son los tipos de propiedades más rentadas?
+		String query = "";
+		return DBConnection.getStatement().executeQuery(query);
+	}
+	
+	public static ResultSet totRentasConImpuestosPorPeriodo() throws SQLException {
+		//Totales de rentas por periodo con impuestos
+		String query = "";
+		return DBConnection.getStatement().executeQuery(query);
+	}
+	
+	public static ResultSet totComisionesPagadasPorPeriodos() throws SQLException {
+		//Totales de comisiones pagadas por periodo
+		String query = "";
+		return DBConnection.getStatement().executeQuery(query);
 	}
 }
