d6b04caec044ea36d6aab306d13633dcc5e7558e
joseManuel1234 <jmanuelrardilla@gmail.com>
2022-11-17 19:24:32 -0500

    a

--- src/application/perfilController.java
+++ src/application/perfilController.java
@@ -3,12 +3,23 @@ package application;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class perfilController {
 
 	@FXML
     private Button guardar;
+	@FXML
+    private TextField textfieldUsuario;
+	@FXML
+    private TextField textfieldNombre;
+	@FXML
+    private TextField textfieldApeliido;
+	@FXML
+    private TextField textfieldCorreo;
+	@FXML
+    private TextField textfieldTipoCuenta;
 	
 	public void Guardar(MouseEvent event)
 	{
