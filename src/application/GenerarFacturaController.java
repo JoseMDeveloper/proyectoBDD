5582eaddfc0c4d5fb13ace57e258c1ce5924ea53
joseManuel1234 <jmanuelrardilla@gmail.com>
2022-11-22 17:53:56 -0500

    a

--- src/application/GenerarFacturaController.java
+++ src/application/GenerarFacturaController.java
@@ -1,19 +1,32 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import dataClass.tipoPago;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
 import javafx.scene.control.Label;
-
+import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class GenerarFacturaController implements Initializable{
 	@FXML
 	private Button agregarPago;
+	@FXML
+	private ListView<tipoPago> listView;
 	@FXML
 	private Label valorPagar;
 	@FXML
@@ -24,9 +37,13 @@ public class GenerarFacturaController implements Initializable{
 	private Button meses;
 	@FXML
 	private Button pagar;
+	@FXML
+	private Text cantMeses;
 	
-	private Integer cantMeses=1;
+	private Integer cantMese=1;
 	private Float totalPagar;
+	private List<tipoPago> paguitos=new ArrayList<>();
+	 private ObservableList<tipoPago> tablita;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -35,22 +52,43 @@ public class GenerarFacturaController implements Initializable{
 	
 	@FXML
 	public void menosMeses(MouseEvent event) {
-		if(cantMeses==2) {
-			cantMeses--;
-			meses.setText(cantMeses+"");
+		if(cantMese==2) {
+			cantMese--;
+			cantMeses.setText(cantMese+"");
 			menosMeses.setDisable(true);
-		}else if(cantMeses>1) {
-			cantMeses--;
-			meses.setText(cantMeses+"");
+		}else if(cantMese>1) {
+			cantMese--;
+			cantMeses.setText(cantMese+"");
 		}
 	}
 	
 	@FXML
 	public void masMeses(MouseEvent event) {
-		cantMeses++;
-		meses.setText(cantMeses+"");
-		if(cantMeses==2) {
+		cantMese++;
+		cantMeses.setText(cantMese+"");
+		if(cantMese==2) {
 			menosMeses.setDisable(false);
 		}
+	}
+	@FXML
+	public void agregar(MouseEvent event) throws IOException 
+	{
+		FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("/source/MetodoPago.fxml"));
+		Parent root1=(Parent)fxmlLoader.load();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root1));
+		stage.centerOnScreen();
+		stage.showAndWait();
+		MetodoPagoController controlador1= fxmlLoader.getController();
+		paguitos.addAll(controlador1.getpaguitos());
+		tablita=FXCollections.observableArrayList(paguitos);
+		listView.setItems(tablita);
+		
+		
+	}
+	@FXML
+	public void pago(MouseEvent event)
+	{
+		
 	}
 }
